e118fcd5de8b257463c0234cae9bbd69b4588b92
Stian Bakken <sbakken@gmail.com>
2024-10-22 15:39:15 +0200

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import deno from "@deno/astro-adapter";
 
 import solidJs from "@astrojs/solid-js";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: deno(),
-  integrations: [solidJs()],
+  integrations: [solidJs(), tailwind()],
 });

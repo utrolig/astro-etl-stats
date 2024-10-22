14c90ca598e9c67ec16ef9ba3c285fe629fd54cb
Stian Bakken <sbakken@gmail.com>
2024-10-22 16:30:50 +0200

    add tailwind theme etc

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,30 @@
+import tailwindForms from "@tailwindcss/forms";
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Noto Sans Variable", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        etl: {
+          text: "hsl(var(--etl-text) / <alpha-value>)",
+          "text-dark": "hsl(var(--etl-text-dark) / <alpha-value>)",
+          bg: "hsl(var(--etl-bg) / <alpha-value>)",
+          100: "hsl(var(--etl-100) / <alpha-value>)",
+          200: "hsl(var(--etl-200) / <alpha-value>)",
+          300: "hsl(var(--etl-300) / <alpha-value>)",
+          400: "hsl(var(--etl-400) / <alpha-value>)",
+          blue: "hsl(var(--etl-blue) / <alpha-value>)",
+          pink: "hsl(var(--etl-pink) / <alpha-value>)",
+          link: "hsl(var(--etl-link) / <alpha-value>)",
+          "link-hover": "hsl(var(--etl-link-hover) / <alpha-value>)",
+        },
+      },
+    },
+  },
+  plugins: [tailwindForms],
+};

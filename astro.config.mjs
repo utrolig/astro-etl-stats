133afe6d886ed203628ec3cfda3b0db253aba8b4
Stian Bakken <sbakken@gmail.com>
2024-10-25 15:18:26 +0200

    Try cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
+import cloudflare from '@astrojs/cloudflare'
 import { defineConfig } from 'astro/config'
-import deno from '@deno/astro-adapter'
-
 import solidJs from '@astrojs/solid-js'
-
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: deno(),
+  adapter: cloudflare(),
   integrations: [solidJs(), tailwind()],
 })
